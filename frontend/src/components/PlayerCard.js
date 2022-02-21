5d84c7a4671444cd1f4700ab5eab12452f28375e
wilfried.maudonpro@gmail.com <wilfried.maudonpro@gmail.com>
2022-02-21 10:32:06 +0100

    card ui updated

--- frontend/src/components/PlayerCard.js
+++ frontend/src/components/PlayerCard.js
@@ -1,23 +1,22 @@
 import React from 'react'
-
+import { Container, Row, Col } from 'react-bootstrap'
 
 const PlayerCard = () => {
     
   return (
-    <div id="topps86" className="cardRow">
-        <div className="card">
-            <div className="photo">
-            <div className="overlay">Q</div>      
-            </div>    
-            <div className="name">TOM BRADY</div>
-            <div className="name2">BUCS</div>
-            <div className="shine"></div>
-        </div>
-        <span className="series"><br/><br/>1986 Topps</span>
-
-        
-    </div>
-
+    <Container>
+        <Row>
+          <div id="topps86" className="cardRow">
+              <div className="card">
+                  <img className="photo" src="https://media4.giphy.com/media/uZmwmfbxh3xkiI1pFM/giphy.gif?cid=ecf05e47kjh0ewnjjatb4cxquw2k8ik0y90q432sms45sqa3&rid=giphy.gif&ct=g" />   
+                  <div className="name">TOM BRADY</div>
+                  <div className="name2">BUCS</div>
+                  <div className="shine"></div>
+              </div>
+              <span className="series"><br/><br/>1986 Topps</span>
+            </div>
+          </Row> 
+    </Container>
   )
 }
 
